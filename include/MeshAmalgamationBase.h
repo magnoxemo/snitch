3873f7f486a62f5b237ba8b33bdfb8c6a6744f96
magnoxemo <walid.ahammed01@gmail.com>
2025-03-03 10:55:41 -0600

    renaming the variable_name and the system_name

--- include/MeshAmalgamationBase.h
+++ include/MeshAmalgamationBase.h
@@ -2,20 +2,20 @@
 #define MESHAMALGAMATION_H
 
 #include <cstdlib>
-#include <vector>
 #include <stdexcept>
+#include <vector>
 
 namespace libMesh {
-    class DofMap;
-    class Elem;
-    class EquationSystems;
-    class LinearImplicitSystem;
-    class Mesh;
-    class NumericVector;
-    class System;
-}
+class DofMap;
+class Elem;
+class EquationSystems;
+class LinearImplicitSystem;
+class Mesh;
+class NumericVector;
+class System;
+} // namespace libMesh
 
-class MeshAmalgamation{
+class MeshAmalgamation {
 
 private:
 protected:
@@ -23,7 +23,7 @@ protected:
   unsigned int _extra_element_integer_index;
   unsigned int _variable_index;
   std::string _system_name;
-  unsigned int _number_of_colors= 12;
+  unsigned int _number_of_colors = 12;
 
   libMesh::Mesh &_mesh;
   libMesh::EquationSystems &_equation_system;
@@ -33,9 +33,10 @@ protected:
   double getElementDataFromMesh(const libMesh::Elem *elem);
 
 public:
-    MeshAmalgamation(libMesh::Mesh &mesh,
-                     libMesh::EquationSystems &equation_system,
-                     const std::string system_name,const std::string variable_name);
+  MeshAmalgamation(libMesh::Mesh &mesh,
+                   libMesh::EquationSystems &equation_system,
+                   const std::string system_name,
+                   const std::string variable_name);
 
   virtual bool belongToCluster(libMesh::Elem *elem,
                                libMesh::Elem *neighbor_elem) = 0;
