3d42453e1585546723f0b11ac962ea562f327a71
magnoxemo <walid.ahammed01@gmail.com>
2025-03-03 15:58:32 -0600

    null

--- test_cases/testLocalMeanHeuristics.cpp
+++ test_cases/testLocalMeanHeuristics.cpp
@@ -9,27 +9,6 @@ double f(double x, double y) {
 }
 
 
-class testLocalMeanHeuristics:public LocalMeanHeuristics{
-public:
-    testLocalMeanHeuristics(libMesh::Mesh &mesh,
-                            libMesh::EquationSystems &equation_system,
-                            std::string system_name): LocalMeanHeuristics(mesh, equation_system, system_name) {}
-    void populatePolynomialField(double (*f)(double,double));
-};
-
-
-void testLocalMeanHeuristics::populatePolynomialField(double (*f)(double,double)){
-    std::vector<libMesh::dof_id_type> dof_indices;
-    for (const auto &elem : _mesh.element_ptr_range()) {
-        _dof_map.dof_indices(elem, dof_indices);
-        libMesh::Point p = elem->vertex_average();
-        double result = f(p(0), p(1));
-        _system.solution->set(dof_indices[0], result);
-    }
-    _system.solution->close();
-}
-
-
 
 
 int main(int argc, char **argv){
