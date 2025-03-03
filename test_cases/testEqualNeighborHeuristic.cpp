413c3b6648efa3463b2a6b1120d8927b3e4e7ca0
magnoxemo <walid.ahammed01@gmail.com>
2025-03-03 12:16:24 -0600

    fixing  typo

--- test_cases/testEqualNeighborHeuristic.cpp
+++ test_cases/testEqualNeighborHeuristic.cpp
@@ -8,11 +8,24 @@
 #include "libmesh/mesh_generation.h"
 
 void PopulateRandomIntegers(libMesh::Mesh &mesh,
-                            libMesh::LinearImplicitSystem &system) {
+                            libMesh::EquationSystems &equation_system,
+                            const std::string system_name,
+                            const std::string variable_name,needs_re_init=false) {
+
+  libMesh::LinearImplicitSystem system =
+      equation_system.add_system<libMesh::LinearImplicitSystem>(system_name);
+
+  system.add_variable(variable_name, libMesh::CONSTANT, libMesh::MONOMIAL);
+  if (needs_re_init){
+      equation_system.re_init();
+  }
+  else{
+      equation_system.init();
+  }
+
   libMesh::DofMap &dof_map = system.get_dof_map();
   std::vector<libMesh::dof_id_type> dof_indices;
   for (const auto &elem : mesh.element_ptr_range()) {
-
     dof_map.dof_indices(elem, dof_indices);
     int random_int = rand() % 5;
     system.solution->set(dof_indices[0], 5 - random_int);
@@ -20,21 +33,11 @@ void PopulateRandomIntegers(libMesh::Mesh &mesh,
   system.solution->close();
 }
 
-void AddVariableToSystem(libMesh::EquationSystems &equation_system,
-                         libMesh::LinearImplicitSystem &system,
-                         std::string var_name, bool needs_re_init = false) {
-  system.add_variable(var_name, libMesh::CONSTANT, libMesh::MONOMIAL);
-  if (!needs_re_init) {
-    equation_system.init();
-  } else {
-    equation_system.reinit();
-  }
-}
 int main(int argc, char **argv) {
-  //declaring the system name and variable_name
-  //variable_name would be the variable based on which heuristics would be done
+  // declaring the system name and variable_name
+  // variable_name would be the variable based on which heuristics would be done
   std::string name_of_the_system = "random_solution_field";
-  std::string populated_varibale_name = "random_data";
+  std::string populated_variable_name = "random_data";
 
   // preparing the mesh and the data
   libMesh::LibMeshInit init(argc, argv);
@@ -44,19 +47,12 @@ int main(int argc, char **argv) {
   libMesh::MeshTools::Generation::build_square(mesh, nx, ny, -10.0, 10.0, -10.0,
                                                10.0);
   libMesh::EquationSystems equation_system(mesh);
-  libMesh::LinearImplicitSystem &system =
-      equation_system.add_system<libMesh::LinearImplicitSystem>(
-          name_of_the_system);
-  AddVariableToSystem(equation_system, system, populated_varibale_name);
-  PopulateRandomIntegers(mesh, system);
+  PopulateRandomIntegers(mesh, equation_system, name_of_the_system,
+                         populated_variable_name);
 
   // actual clustering process
-  /* Now the framework presumes I have the mesh and equation_system
-   * and I am fully aware what is the name and type of the system (for now we
-   * are only allowing LinearImplicitSystem) which solution field (name of the
-   * variable) should be */
   EqualNeighborHeuristic demo(mesh, equation_system, name_of_the_system,
-                              populated_varibale_name);
+                              populated_variable_name);
   demo.setTolerance(0.001);
   demo.findCluster();
   demo.captureClusterID();
