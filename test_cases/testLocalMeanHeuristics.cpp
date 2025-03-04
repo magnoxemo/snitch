5ae4375a0420cbcbf94767a5d8df548f075a27fc
magnoxemo <walid.ahammed01@gmail.com>
2025-03-04 10:41:30 -0600

    adding clang format

--- test_cases/testLocalMeanHeuristics.cpp
+++ test_cases/testLocalMeanHeuristics.cpp
@@ -1,64 +1,65 @@
-#include <cmath>
-#include<cstdlib>
-#include "libmesh/mesh_generation.h"
 #include "libmesh/libmesh.h"
+#include "libmesh/mesh_generation.h"
+#include <cmath>
+#include <cstdlib>
 
 #include "LocalMeanHeuristics.h"
 
-
-double f(double x, double y) {
-    return x*x/100-2*x*y/100-sin(exp(x)-4*cos(x*y));
+double f(libMesh::Point &point) {
+  // x = point (0)
+  // y = point (1)
+  return point(0) * point(0) / 100 - 2 * point(0) * point(1) / 100 -
+         sin(exp(point(0)) - 4 * cos(point(0) * point(0)));
 }
 
-void PopulatePolynomailField(libMesh::Mesh &mesh,
-                            libMesh::EquationSystems &equation_system,
-                            const std::string system_name,
-                            const std::string variable_name,bool needs_re_init=false) {
+void PopulatePolynomialField(libMesh::Mesh &mesh,
+                             libMesh::EquationSystems &equation_system,
+                             const std::string system_name,
+                             const std::string variable_name,
+                             bool needs_re_init = false) {
 
-    libMesh::LinearImplicitSystem& system =equation_system.add_system<libMesh::LinearImplicitSystem>(system_name);
+  libMesh::LinearImplicitSystem &system =
+      equation_system.add_system<libMesh::LinearImplicitSystem>(system_name);
 
-    system.add_variable(variable_name, libMesh::CONSTANT, libMesh::MONOMIAL);
-    if (needs_re_init){
-        equation_system.reinit();
-    }
-    else{
-        equation_system.init();
-    }
+  system.add_variable(variable_name, libMesh::CONSTANT, libMesh::MONOMIAL);
+  if (needs_re_init) {
+    equation_system.reinit();
+  } else {
+    equation_system.init();
+  }
 
-    libMesh::DofMap &dof_map = system.get_dof_map();
-    std::vector<libMesh::dof_id_type> dof_indices;
-    for (const auto &elem : mesh.element_ptr_range()) {
-        dof_map.dof_indices(elem, dof_indices);
-        libMesh::Point p = elem->vertex_average();
-        double result = f(p(0), p(1));
-        system.solution->set(dof_indices[0],result);
-    }
-    system.solution->close();
+  libMesh::DofMap &dof_map = system.get_dof_map();
+  std::vector<libMesh::dof_id_type> dof_indices;
+  for (const auto &elem : mesh.element_ptr_range()) {
+    dof_map.dof_indices(elem, dof_indices);
+    libMesh::Point p = elem->vertex_average();
+    system.solution->set(dof_indices[0], f(&p););
+  }
+  system.solution->close();
 }
 
+int main(int argc, char **argv) {
 
-int main(int argc, char **argv){
+  std::string name_of_the_system = "polynomial_solution_field";
+  std::string populated_variable_name = "polynomial_data";
 
-    std::string name_of_the_system = "polynomial_solution_field";
-    std::string populated_variable_name = "polynomial_data";
+  libMesh::LibMeshInit init(argc, argv);
+  libMesh::Mesh mesh(init.comm());
+  unsigned int nx = 500;
+  unsigned int ny = 500;
 
-    libMesh::LibMeshInit init(argc, argv);
-    libMesh::Mesh mesh(init.comm());
-    unsigned int nx = 500;
-    unsigned int ny = 500;
+  libMesh::MeshTools::Generation::build_square(mesh, nx, ny, -10.0, 10.0, -10.0,
+                                               10.0);
+  libMesh::EquationSystems equation_system(mesh);
+  PopulatePolynomialField(mesh, equation_system, name_of_the_system,
+                          populated_variable_name);
 
-    libMesh::MeshTools::Generation::build_square(mesh, nx, ny, -10.0, 10.0, -10.0,
-                                                 10.0);
-    libMesh::EquationSystems equation_system(mesh);
-    PopulatePolynomailField(mesh, equation_system, name_of_the_system,
-                           populated_variable_name);
+  LocalMeanHeuristics quadratic_field(mesh, equation_system, name_of_the_system,
+                                      populated_variable_name);
+  quadratic_field.setTolerance(0.1);
+  quadratic_field.findCluster();
+  quadratic_field.captureClusterID();
+  quadratic_field.writeOutputData("output.e");
 
-    LocalMeanHeuristics quadratic_field (mesh,equation_system, name_of_the_system,
-                                             populated_variable_name);
-    quadratic_field.setTolerance(0.1);
-    quadratic_field.findCluster();
-    quadratic_field.captureClusterID();
-    quadratic_field.writeOutputData("output.e");
-
-    return 0;
+  return 0;
 }
