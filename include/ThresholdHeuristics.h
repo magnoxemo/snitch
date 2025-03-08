37e843a83cf7055be5c36777ee37b57e49146d3f
magnoxemo <walid.ahammed01@gmail.com>
2025-03-07 19:17:37 -0600

    clang format

--- include/ThresholdHeuristics.h
+++ include/ThresholdHeuristics.h
@@ -3,20 +3,22 @@
 #include "MeshAmalgamationBase.h"
 class ThresholdHeuristics : public MeshAmalgamation {
 private:
-  double _threshold =0.05;
+  double _threshold = 0.05;
   bool _value_crosses_threshold = true;
+
 protected:
 public:
-    ThresholdHeuristics(libMesh::Mesh& mesh,
-                              libMesh::EquationSystems &equation_system,
-                              const std::string system_name,
-                              const std::string variable_name)
+  ThresholdHeuristics(libMesh::Mesh &mesh,
+                      libMesh::EquationSystems &equation_system,
+                      const std::string system_name,
+                      const std::string variable_name)
       : MeshAmalgamation(mesh, equation_system, system_name, variable_name) {}
   bool belongToCluster(libMesh::Elem *elem,
                        libMesh::Elem *neighbor_elem) override;
   void setThreshold(double threshold) { _threshold = threshold; }
-  void setComparisonMode (bool value_crosses_threshold) { _value_crosses_threshold =value_crosses_threshold; }
-
+  void setComparisonMode(bool value_crosses_threshold) {
+    _value_crosses_threshold = value_crosses_threshold;
+  }
 };
 
 #endif
