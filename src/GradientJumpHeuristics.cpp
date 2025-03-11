87f73184df618a919978e3305eb422816bbac6cb
magnoxemo <walid.ahammed01@gmail.com>
2025-03-11 11:45:54 -0500

    typo

--- src/GradientJumpHeuristics.cpp
+++ src/GradientJumpHeuristics.cpp
@@ -1,6 +1,6 @@
 #include "GradientJumpHeuristics.h"
 
-bool ThresholdHeuristics::belongToCluster(libMesh::Elem *elem,
+bool GradientJumpHeuristics::belongToCluster(libMesh::Elem *elem,
                                           libMesh::Elem *neighbor_elem) {
     double jump_diff = std::abs(calculateGradientJump(elem) -
                                 calculateGradientJump(neighbor_elem));
@@ -13,7 +13,7 @@ bool ThresholdHeuristics::belongToCluster(libMesh::Elem *elem,
                getElementDataFromMesh(neighbor_elem) < _threshold));
 }
 
-double ThresholdHeuristics::calculateGradientJump(libMesh::Elem *elem) {
+double GradientJumpHeuristics::calculateGradientJump(libMesh::Elem *elem) {
     double j_e = 0;
     libMesh::Point p_e = elem->vertex_average();
     double element_solution = getElementDataFromMesh(elem);
@@ -21,8 +21,7 @@ double ThresholdHeuristics::calculateGradientJump(libMesh::Elem *elem) {
         libMesh::Elem *current_elem = elem->neighbor_ptr(s);
         if (current_elem) {
             libMesh::Point p_s = current_elem->vertex_average();
-            j_e +=
-                    std::abs((element_solution - getElementDataFromMesh(current_elem)) /
+            j_e +=std::abs((element_solution - getElementDataFromMesh(current_elem)) /
                              (calculateDistance(p_e, p_s)));
         }
     }
@@ -30,7 +29,7 @@ double ThresholdHeuristics::calculateGradientJump(libMesh::Elem *elem) {
     return j_e;
 }
 
-double ThresholdHeuristics::calculateDistance(libMesh::Point &p1,
+double GradientJumpHeuristics::calculateDistance(libMesh::Point &p1,
                                               libMesh::Point &p2) {
     return sqrt((p1(0) - p2(0)) * (p1(0) - p2(0)) +
                 (p1(1) - p2(1)) * (p1(1) - p2(1)));
