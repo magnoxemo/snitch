3c92fc67dd62de0258de190f791bb916a98955bf
magnoxemo <walid.ahammed01@gmail.com>
2025-03-18 09:20:48 -0500

    applying suggestions

--- include/BasicUtility.h
+++ include/BasicUtility.h
@@ -1,20 +1,14 @@
 #ifndef BASICUTILITY_H
 #define BASICUTILITY_H
 
-#include "libmesh/dof_map.h"
-#include "libmesh/elem.h"
 #include "libmesh/equation_systems.h"
-#include "libmesh/libmesh.h"
-#include "libmesh/linear_implicit_system.h"
 #include "libmesh/mesh.h"
-#include "libmesh/numeric_vector.h"
-#include "libmesh/system.h"
 
 void PopulateSyntheticData(libMesh::Mesh &mesh,
-                      libMesh::EquationSystems &equation_system,
-                      const std::string system_name,
-                      const std::string variable_name,
-                      double (*CalculateData)(const libMesh::Point&),
-                      bool needs_re_init=false) ;
+                           libMesh::EquationSystems &equation_system,
+                           const std::string system_name,
+                           const std::string variable_name,
+                           double (*CalculateData)(const libMesh::Point &),
+                           bool needs_re_init = false);
 
 #endif
--- test_cases/testStepFunction.cpp
+++ test_cases/testStepFunction.cpp
@@ -1,33 +1,33 @@
-#include "EqualNeighborHeuristic.h"
 #include "BasicUtility.h"
+#include "EqualNeighborHeuristic.h"
 
 #include "libmesh/libmesh.h"
 #include "libmesh/mesh_generation.h"
 
-#define dx 0.5;
-
-double CalculateStep_x(const libMesh::Point & p){
-    double x = p(0);
-    return int (x)/dx;
+double CalculateStep_x(const libMesh::Point &p) {
+  double dx = 0.5;
+  double x = p(0);
+  return int(x) / dx;
 }
 
 int main(int argc, char **argv) {
-    std::string name_of_the_system = "step_function";
-    std::string populated_variable_name = "step_function_data";
+  std::string name_of_the_system = "step_function";
+  std::string populated_variable_name = "step_function_data";
 
-    libMesh::LibMeshInit init(argc, argv);
-    libMesh::Mesh mesh(init.comm());
-    unsigned int nx = 10;
-    unsigned int ny = 10;
-    libMesh::MeshTools::Generation::build_square(mesh, nx, ny, -10.0, 10.0, -10.0,
-                                                 10.0);
-    libMesh::EquationSystems equation_system(mesh);
-    PopulateSyntheticData(mesh, equation_system, name_of_the_system, populated_variable_name, CalculateStep_x);
+  libMesh::LibMeshInit init(argc, argv);
+  libMesh::Mesh mesh(init.comm());
+  unsigned int nx = 10;
+  unsigned int ny = 10;
+  libMesh::MeshTools::Generation::build_square(mesh, nx, ny, -10.0, 10.0, -10.0,
+                                               10.0);
+  libMesh::EquationSystems equation_system(mesh);
+  PopulateSyntheticData(mesh, equation_system, name_of_the_system,
+                        populated_variable_name, CalculateStep_x);
 
-    EqualNeighborHeuristic demo(mesh, equation_system, name_of_the_system, populated_variable_name);
-    demo.setTolerance(0.001);
-    demo.findCluster();
-    demo.captureClusterID();
-    demo.writeOutputData("output.e");
+  EqualNeighborHeuristic demo(mesh, equation_system, name_of_the_system,
+                              populated_variable_name);
+  demo.setTolerance(0.001);
+  demo.findCluster();
+  demo.captureClusterID();
+  demo.writeOutputData("output.e");
 }
-
