04acda44bc7d934944066e9312c5b1d5a11e199d
magnoxemo <walid.ahammed01@gmail.com>
2025-03-18 09:20:48 -0500

    applying suggestions

--- src/BasicUtility.cpp
+++ src/BasicUtility.cpp
@@ -1,30 +1,36 @@
 #include "BasicUtility.h"
 
+#include "libmesh/dof_map.h"
+#include "libmesh/elem.h"
+#include "libmesh/libmesh.h"
+#include "libmesh/linear_implicit_system.h"
+#include "libmesh/numeric_vector.h"
+#include "libmesh/system.h"
+
 void PopulateSyntheticData(libMesh::Mesh &mesh,
-                      libMesh::EquationSystems &equation_system,
-                      const std::string system_name,
-                      const std::string variable_name,
-                      double (*CalculateData)(const libMesh::Point&),
-                      bool needs_re_init) {
+                           libMesh::EquationSystems &equation_system,
+                           const std::string system_name,
+                           const std::string variable_name,
+                           double (*CalculateData)(const libMesh::Point &),
+                           bool needs_re_init) {
 
-    libMesh::LinearImplicitSystem& system =
-            equation_system.add_system<libMesh::LinearImplicitSystem>(system_name);
+  libMesh::LinearImplicitSystem &system =
+      equation_system.add_system<libMesh::LinearImplicitSystem>(system_name);
 
-    system.add_variable(variable_name, libMesh::CONSTANT, libMesh::MONOMIAL);
-    if (needs_re_init){
-        equation_system.reinit();
-    }
-    else{
-        equation_system.init();
-    }
+  system.add_variable(variable_name, libMesh::CONSTANT, libMesh::MONOMIAL);
+  if (needs_re_init) {
+    equation_system.reinit();
+  } else {
+    equation_system.init();
+  }
 
-    libMesh::DofMap &dof_map = system.get_dof_map();
-    std::vector<libMesh::dof_id_type> dof_indices;
-    for (const auto &elem : mesh.element_ptr_range()) {
-        dof_map.dof_indices(elem, dof_indices);
-        libMesh::Point p = elem->vertex_average();
-        double score = CalculateData(p);
-        system.solution->set(dof_indices[0], score );
-    }
-    system.solution->close();
+  libMesh::DofMap &dof_map = system.get_dof_map();
+  std::vector<libMesh::dof_id_type> dof_indices;
+  for (const auto &elem : mesh.element_ptr_range()) {
+    dof_map.dof_indices(elem, dof_indices);
+    libMesh::Point p = elem->vertex_average();
+    double score = CalculateData(p);
+    system.solution->set(dof_indices[0], score);
+  }
+  system.solution->close();
 }
