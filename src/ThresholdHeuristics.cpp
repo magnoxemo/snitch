71a16cc87dabdeed7f4070231ae0250e952acb5c
magnoxemo <walid.ahammed01@gmail.com>
2025-03-19 15:48:49 -0500

    clang format

--- src/ThresholdHeuristics.cpp
+++ src/ThresholdHeuristics.cpp
@@ -2,9 +2,9 @@
 
 bool ThresholdHeuristics::belongToCluster(libMesh::Elem *elem,
                                           libMesh::Elem *neighbor_elem) {
-    return _value_crosses_threshold
-           ? ((getElementDataFromMesh(elem) > _threshold &&
-               getElementDataFromMesh(neighbor_elem) > _threshold))
-           : ((getElementDataFromMesh(elem) < _threshold &&
-               getElementDataFromMesh(neighbor_elem) < _threshold));
+  return _value_crosses_threshold
+             ? ((getElementDataFromMesh(elem) > _threshold &&
+                 getElementDataFromMesh(neighbor_elem) > _threshold))
+             : ((getElementDataFromMesh(elem) < _threshold &&
+                 getElementDataFromMesh(neighbor_elem) < _threshold));
 }
