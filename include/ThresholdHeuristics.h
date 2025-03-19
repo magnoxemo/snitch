02c3d0211e42964c1ccbaeffa827d40dd4204fad
magnoxemo <walid.ahammed01@gmail.com>
2025-03-19 15:46:43 -0500

    adding standard deviation heuristics

---
+++ include/ThresholdHeuristics.h
@@ -0,0 +1,27 @@
+#ifndef THRESHOLDHEURISTICS_H
+#define THRESHOLDHEURISTICS_H
+
+#include "MeshAmalgamationBase.h"
+
+class ThresholdHeuristics : public MeshAmalgamation {
+
+private:
+    double _threshold = 0.05;
+
+protected:
+    bool _value_crosses_threshold = true;
+
+public:
+    ThresholdHeuristics(libMesh::EquationSystems &equation_system,
+                        const std::string system_name,
+                        const std::string variable_name)
+            : MeshAmalgamation(equation_system, system_name, variable_name) {}
+    virtual bool belongToCluster(libMesh::Elem *elem,
+                                 libMesh::Elem *neighbor_elem) override;
+    void setThreshold(double threshold) { _threshold = threshold; }
+    void setComparisonMode(bool value_crosses_threshold) {
+        _value_crosses_threshold = value_crosses_threshold;
+    }
+};
+
+#endif
