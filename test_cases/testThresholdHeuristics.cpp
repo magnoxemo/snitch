f4c8f725d62b98bd57ecf235237ca034708b979a
magnoxemo <walid.ahammed01@gmail.com>
2025-03-19 16:12:54 -0500

    formating code

--- test_cases/testThresholdHeuristics.cpp
+++ test_cases/testThresholdHeuristics.cpp
@@ -1,24 +1,24 @@
+#include <cmath>
+#include <cstdlib>
+
+#include "libmesh/libmesh.h"
 
 #include "BasicUtility.h"
 #include "ThresholdHeuristics.h"
 
-#include "libmesh/libmesh.h"
-#include <cmath>
-#include <cstdlib>
-
-#define alpha 0.01
-#define sigma_t_fuel 0.0332
-#define sigma_t_mod 0.052
+#define ALPHA 0.01
+#define SIGMA_T_FUEL 0.0332
+#define SIGMA_T_MOD 0.052
 
 double standard_dev(const libMesh::Point &point) {
 
   double r = sqrt(point(0) * point(0) + point(1) * point(1));
-  double sigma = sigma_t_fuel;
+  double sigma = SIGMA_T_FUEL;
 
   if (r > 5)
-    sigma = sigma_t_mod;
+    sigma = SIGMA_T_MOD;
 
-  return alpha * pow(sigma, -sigma * r);
+  return ALPHA * pow(sigma, -sigma * r);
 }
 
 int main(int argc, char **argv) {
@@ -26,10 +26,13 @@ int main(int argc, char **argv) {
   std::string name_of_the_system = "polynomial_solution_field";
   std::string populated_variable_name = "polynomial_data";
   std::string populated_std_variable_name = "std_data";
+
   libMesh::LibMeshInit init(argc, argv);
   libMesh::Mesh mesh(init.comm());
   BuildMesh(argv, mesh);
+
   libMesh::EquationSystems equation_system(mesh);
+
   PopulateSyntheticData(equation_system, name_of_the_system,
                         populated_std_variable_name, standard_dev);
 
